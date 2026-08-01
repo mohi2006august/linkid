20dabf1ff28c57772dcf1425565ad5da3468b3ae
HirenixAI <nizamuddinnumaan822@gmail.com>
2026-08-01 19:41:03 +0530

    fix: use online migration strategy for link groups

--- prisma/migrations/20260801_add_link_groups/migration.sql
+++ prisma/migrations/20260801_add_link_groups/migration.sql
@@ -2,8 +2,5 @@
 ALTER TABLE "Link" ADD COLUMN "isGroup" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN "parentId" TEXT;
 
--- CreateIndex
-CREATE INDEX "Link_parentId_idx" ON "Link"("parentId");
-
--- AddForeignKey
-ALTER TABLE "Link" ADD CONSTRAINT "Link_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Link"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+-- AddForeignKey (Not Valid initially)
+ALTER TABLE "Link" ADD CONSTRAINT "Link_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Link"("id") ON DELETE SET NULL ON UPDATE CASCADE NOT VALID;
